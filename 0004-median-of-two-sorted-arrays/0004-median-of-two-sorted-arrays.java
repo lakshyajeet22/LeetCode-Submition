545b1ea8825b2be180814c4854159ab7ed40dd81
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-01 00:21:50 +0530

    Time: 1 ms (100%), Space: 48.6 MB (93.36%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,57 +1,35 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-
-        if(nums1.length > nums2.length)
-            return findMedianSortedArrays(nums2, nums1);
-
-        int m = nums1.length;
-        int n = nums2.length;
-
-        int low = 0;
-        int high = m;
-
-        while(low <= high) {
-
-            int cut1 = (low + high) / 2;
-            int cut2 = (m + n + 1) / 2 - cut1;
-
-            int left1 =
-                (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
-
-            int left2 =
-                (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
-
-            int right1 =
-                (cut1 == m) ? Integer.MAX_VALUE : nums1[cut1];
-
-            int right2 =
-                (cut2 == n) ? Integer.MAX_VALUE : nums2[cut2];
-
-            if(left1 <= right2 && left2 <= right1) {
-
-                if((m + n) % 2 == 0) {
-
-                    return (
-                        Math.max(left1, left2)
-                        +
-                        Math.min(right1, right2)
-                    ) / 2.0;
-
-                } else {
-
-                    return Math.max(left1, left2);
-                }
-
-            } else if(left1 > right2) {
-
-                high = cut1 - 1;
-
-            } else {
-
-                low = cut1 + 1;
-            }
+       
+        
+        if(nums1.length>nums2.length){
+            int[] temp= nums1;
+            nums1=nums2;
+            nums2=temp;
         }
+        int m=nums1.length, n=nums2.length;
+         int s=0, e=m;
+        // if(nums1.length>nums2.length)  findMedianSortedArrays(nums2, nums1);
 
-        return 0.0;
+        while(s<=e){
+            int mid1 = s+(e-s)/2;
+            int mid2 = (m+n+1)/2 - mid1;
+
+            int l1=mid1==0?Integer.MIN_VALUE: nums1[mid1-1];
+            int l2=mid2==0? Integer.MIN_VALUE: nums2[mid2-1];
+
+            int r1= mid1==m?Integer.MAX_VALUE: nums1[mid1];
+            int r2=mid2==n? Integer.MAX_VALUE: nums2[mid2];
+
+            if(l1<=r2 && l2<=r1){
+                if((n+m)%2==0){
+                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
+                }else return Math.max(l1,l2);
+            }if(l1>r2){
+                e=mid1-1;
+            }else s=mid1+1;
+
+            
+        }return 0.0;
     }
 }
