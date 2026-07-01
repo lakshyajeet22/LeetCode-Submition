0f8e00559e830b59098fea61a34a17af0f4f80c7
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-01 19:31:11 +0530

    Time: 1 ms (100%), Space: 48.3 MB (97.97%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-       
-        
-        // if(nums1.length>nums2.length){
-        //     int[] temp= nums1;
-        //     nums1=nums2;
-        //     nums2=temp;
-        // }
-        int m=nums1.length, n=nums2.length;
-        int s=0, e=m;
-        if(nums1.length>nums2.length)  return findMedianSortedArrays(nums2, nums1);
+        int m = nums1.length;
+        int n = nums2.length;
 
-        while(s<=e){
-            int mid1 = s+(e-s)/2;
-            int mid2 = (m+n+1)/2 - mid1;
+        int[] merged = new int[m + n];
 
-            int l1=mid1==0?Integer.MIN_VALUE: nums1[mid1-1];
-            int l2=mid2==0? Integer.MIN_VALUE: nums2[mid2-1];
+        int i = 0, j = 0, k = 0;
 
-            int r1= mid1==m?Integer.MAX_VALUE: nums1[mid1];
-            int r2=mid2==n? Integer.MAX_VALUE: nums2[mid2];
+        // Merge both arrays
+        while (i < m && j < n) {
+            if (nums1[i] <= nums2[j]) {
+                merged[k++] = nums1[i++];
+            } else {
+                merged[k++] = nums2[j++];
+            }
+        }
 
-            if(l1<=r2 && l2<=r1){
-                if((n+m)%2==0){
-                    return (Math.max(l1,l2) + Math.min(r1,r2))/2.0;
-                }else return Math.max(l1,l2);
-            }if(l1>r2){
-                e=mid1-1;
-            }else s=mid1+1;
+        // Copy remaining elements
+        while (i < m) {
+            merged[k++] = nums1[i++];
+        }
 
-            
-        }return 0.0;
+        while (j < n) {
+            merged[k++] = nums2[j++];
+        }
+
+        int len = m + n;
+
+        // Find median
+        if (len % 2 == 1) {
+            return merged[len / 2];
+        } else {
+            return (merged[len / 2] + merged[len / 2 - 1]) / 2.0;
+        }
     }
 }
