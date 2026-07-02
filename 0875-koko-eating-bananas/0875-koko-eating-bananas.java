3b02c522a49f6ccac4f9ac8ad6a1090932355ed3
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-02 18:47:10 +0530

    Time: 25 ms (23.3%), Space: 47.8 MB (58.24%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,23 @@
+class Solution {
+    public static boolean isValid(int [] arr, int h, int mid){
+       int sum=0;
+       for(int i: arr){
+            sum+=Math.ceil((double)i/mid);  
+       }return sum<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int s=1, e=0, ans=-1;
+        for(int i: piles){
+            e=Math.max(e, i);
+           
+        }
+        while(s<=e){
+            
+            int mid=s+(e-s)/2;
+            if(isValid(piles, h, mid)){
+                e=mid-1;
+                ans=mid;
+            }else s=mid+1;
+        }return ans ;
+    }
+}
