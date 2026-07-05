08ccc6c1040edb6c06abf49e670ee049cba26678
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-05 17:52:17 +0530

    Time: 29 ms (11.19%), Space: 47.7 MB (5.33%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,28 +1,30 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         for (int i = 0; i < nums.length - 3; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue;
-
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
             for (int j = i + 1; j < nums.length - 2; j++) {
-                if (j > i+1 && nums[j] == nums[j-1]) continue;
-                for (int k = j + 1; k < nums.length - 1; k++) {
-                     if (k > j+1 && nums[k] == nums[k-1]) continue;
-                    for(int l =k+1 ; l < nums.length; l++){
-                         if (l > k+1 && nums[l] == nums[l-1]) continue;
-                         long sum = (long)nums[i]+nums[j]+nums[k]+nums[l];
-                        if(sum == target){
-                           List<Integer> temp = new ArrayList<>();
-                           temp.add(nums[i]);
-                           temp.add(nums[j]);
-                           temp.add(nums[k]);
-                           temp.add(nums[l]);
-                           list.add(temp);
-                        }
-                    }
+                if (j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+                int l=j+1;
+                int r=nums.length-1;
+                while(l<r){
+                    List<Integer> list = new ArrayList<>();
+                    long sum= (long) nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum==target){
+                        list.add(nums[i]);
+                        list.add(nums[j]); 
+                        list.add(nums[l++]);
+                        list.add(nums[r--]);
+
+                     if(!ans.contains(list))    ans.add(list);
+
+                    }else if(sum<target) l++;
+                    else r--;
                 }
             }
-        }return list;
+        }return ans;
     }
 }
