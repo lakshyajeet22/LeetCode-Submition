ca5d7ad1fa2ac10ad0ff0c8c2a4ce5f9fa5a58ee
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-10 23:39:56 +0530

    Time: 2 ms (70.26%), Space: 55.4 MB (69.21%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int winner=nums[0], vote=1;
+        for(int i : nums){
+            if(i!=winner){
+                vote--;
+            }else if(i==winner){
+                vote++;
+
+            }if(vote==0){
+                winner=i;
+                vote=1;
+            }
+        }return winner;
+    }
+}
