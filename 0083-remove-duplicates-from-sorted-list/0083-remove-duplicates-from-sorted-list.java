0e1622503acdb99673c7efa988bfbe9c9962f179
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-16 13:11:13 +0530

    Time: 0 ms (100%), Space: 45.8 MB (5.62%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.java
@@ -9,15 +9,26 @@
  * }
  */
 class Solution {
+    public ListNode rec(ListNode head){
+        if(head==null || head.next== null) return head;
+        ListNode ans=rec(head.next);
+        if(ans.val == head.val) return ans;
+        else {
+            head.next=ans;
+            return head;
+        }
+
+    }
     public ListNode deleteDuplicates(ListNode head) {
-        ListNode temp=head;
-        if(head==null || head.next==null) return head;
-        while(temp!=null){
-            if(temp.next!=null && temp.val==temp.next.val){
-                temp.next=temp.next.next;
-            }else{
-                temp=temp.next;
-            }
-        }return head;
+        // ListNode temp=head;
+        // if(head==null || head.next==null) return head;
+        // while(temp!=null){
+        //     if(temp.next!=null && temp.val==temp.next.val){
+        //         temp.next=temp.next.next;
+        //     }else{
+        //         temp=temp.next;
+        //     }
+        // }return head;
+        return rec(head);
     }
 }
