20e5c3e791de9f69f7193201464bae0ab21d6e14
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-07-24 18:11:11 +0530

    Time: 5 ms (13.7%), Space: 44 MB (5.89%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '[' || s.charAt(i) == '{' || s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+                continue;
+
+            }
+            if (stack.isEmpty()) {
+                return false;
+            }
+
+            if (s.charAt(i) == ']' && stack.peek() == '[') {
+                stack.pop();
+                continue;
+            }
+            if (s.charAt(i) == '}' && stack.peek() == '{') {
+                stack.pop();
+                continue;
+            } 
+            if (s.charAt(i) == ')' && stack.peek() == '(') {
+                stack.pop();
+                continue;
+            }
+            return false;
+
+        }
+        if(!stack.isEmpty()) return false;
+        return true;
+    }
+}
