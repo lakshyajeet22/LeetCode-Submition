549917f664c9e7f4ee81a0792898396d6b29d37e
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-08-04 18:47:17 +0530

    Time: 0 ms (100%), Space: 43.3 MB (77.85%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,33 +1,32 @@
 class Solution {
-    public void solve(Map<Character, String> map, String digits, int i, String curr, List<String> ans ){
-        if(i==digits.length()){
-            ans.add(curr);
+    List<String> list;
+    Map<Integer, char[]> map;
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) return new ArrayList<>();
+        list = new ArrayList<>();
+        map = new HashMap<>();
+        map.put(2, new char[]{'a', 'b', 'c'});
+        map.put(3, new char[]{'d', 'e', 'f'});
+        map.put(4, new char[]{'g', 'h', 'i'});
+        map.put(5, new char[]{'j', 'k', 'l'});
+        map.put(6, new char[]{'m', 'n', 'o'});
+        map.put(7, new char[]{'p', 'q', 'r', 's'});
+        map.put(8, new char[]{'t', 'u', 'v'});
+        map.put(9, new char[]{'w', 'x', 'y', 'z'});
+
+        process(0, new StringBuilder(), digits);
+        return list;
+    }
+
+    private void process(int index, StringBuilder sb, String digits){
+        if (index == digits.length()){
+            list.add(sb.toString());
             return;
         }
-        String temp = map.get(digits.charAt(i));
-        for(int j=0; j<temp.length(); j++){
-            solve(map, digits, i+1, curr+temp.charAt(j), ans);
+        for (char c : map.get((digits.charAt(index) - '0'))){
+            sb.append(c);
+            process(index + 1, sb, digits);
+            sb.deleteCharAt(sb.length() - 1);
         }
-
-    }
-    public List<String> letterCombinations(String digits) {
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('6', "mno");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
-        List<String> ans = new ArrayList<>();
-        String curr = "";
-        solve(map, digits, 0, curr, ans );
-        return ans;
-
-
-
-
-        
     }
 }
