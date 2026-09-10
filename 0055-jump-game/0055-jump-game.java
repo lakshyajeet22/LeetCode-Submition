326b73ec52c3f37d45f5832e56bbc1be06a7251b
Lakshyajeet Singh <lakshyajeet.sati.27@gmail.com>
2026-09-10 19:38:50 +0530

    Time: 1 ms (99.94%), Space: 48.1 MB (8.62%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,20 +1,12 @@
 class Solution {
-    boolean solve(int[] nums, int i, Boolean[] dp){
-        if(i>nums.length-1) return false;
-        if(dp[i]!=null) return dp[i];
-        if(i==nums.length-1) return true;
-        if(nums[i]==0) return false;
-        boolean ans = false;
-        for(int j=nums[i]; j>0; j--){
-            ans = (solve(nums, i+j, dp)) ;
-            if(ans)
-                break;    
-        }
-        return dp[i]=ans;
-    }
     public boolean canJump(int[] nums) {
-        Boolean[] dp = new Boolean[nums.length+1];
-        if(solve(nums, 0, dp))  return true;
-        return false;
+        int f=nums.length-1;
+        int n=nums.length;
+        for(int i=n-2; i>=0; i--){
+            if(i+nums[i]>=f){
+                f=i;
+            }
+        }
+        return f==0;
     }
 }
